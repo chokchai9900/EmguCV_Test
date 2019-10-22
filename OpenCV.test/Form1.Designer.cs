b09c0e5981d74abaead9816f231f715a274f0036
chokchai9900 <chokchai9900@gmail.com>
2019-10-22 13:50:26 +0700

    x2tower

---
+++ OpenCV.test/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace OpenCV.test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CameraBox = new Emgu.CV.UI.ImageBox();
+            this.start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Location = new System.Drawing.Point(44, 35);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(347, 289);
+            this.CameraBox.TabIndex = 2;
+            this.CameraBox.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(44, 358);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(180, 52);
+            this.start.TabIndex = 3;
+            this.start.Text = "Detection And Recognition";
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.CameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CameraBox;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ OpenCV.test/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenCV.test
+{
+
+    public partial class Form1 : Form
+    {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainigImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLables, t;
+        string name, names = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+                string FaceLoad;
+                for (int i = 1; i < NumLables; i++)
+                {
+                    FaceLoad = "face" + i + ".bmp";
+                    trainigImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Face.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the Database");
+            }
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, byte>();
+            MCvAvgComp[][] faceDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in faceDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (trainigImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainigImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+
+                }
+                Users[t - 1] = name;
+                Users.Add("");
+            }
+            CameraBox.Image = Frame;
+            name = "";
+            Users.Clear();
+        }
+    }
+}
